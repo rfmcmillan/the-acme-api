c0aeeae5ddd484fadf0f06aad320a24e38e9acdd
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-04-22 15:43:48 -0400

    profs heroku ssl workaround

--- db.js
+++ db.js
@@ -1,5 +1,18 @@
 const Sequelize = require('sequelize');
 const { DataTypes } = Sequelize;
+
+//this is prof's workaround for the SSL error that occurs when deploying to heroku. Alternatively, in the CLI, you can enter 'heroku config:set PGSSLMODE=no-verify -a <your-app's name>'
+
+const config = {};
+
+if (process.env.SSL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost/acme_db'
 );
